bf39405584445dd9e9112bc98e0f7d514ecb2968
Wiktor Kapanowski <wiktor.kapanowski@gmail.com>
2019-10-07 11:02:31 +0200

    Lesson 04, task 2 (version 1) - correction of the main method

--- src/task2/FiguresTest.java
+++ src/task2/FiguresTest.java
@@ -2,7 +2,6 @@ package task2;
 
 public class FiguresTest {
     public static void main(String[] args) {
-        ShapeCalculator shapeCalculator = new ShapeCalculator();
 
         Square square1 = new Square(3.5);
         square1.showSquareArea();
