5631f5e8bcc681a2b7234299c51464347e273da5
Wiktor Kapanowski <wiktor.kapanowski@gmail.com>
2019-10-07 23:26:54 +0200

    Removing "this" where unnecessary.

--- src/task3/Triangle.java
+++ src/task3/Triangle.java
@@ -11,20 +11,20 @@ public class Triangle {
         this.sideC = sideC;
     }
 
-    private boolean areTriangleSidesCorrect(Triangle triangle) {
-        boolean areCorrect = (triangle.sideA + triangle.sideB > triangle.sideC &&
-                triangle.sideA + triangle.sideC > triangle.sideB &&
-                triangle.sideB + triangle.sideC > triangle.sideA);
+    private boolean areTriangleSidesCorrect() {
+        boolean areCorrect = (sideA + sideB > sideC &&
+                sideA + sideC > sideB &&
+                sideB + sideC > sideA);
         return areCorrect;
     }
 
     public void showRightTriangleInfo() {
-        if (areTriangleSidesCorrect(this)) {
+        if (areTriangleSidesCorrect()) {
             TriangleCheck triangleCheck = new TriangleCheck();
             boolean rightTriangle = triangleCheck.isRightTriangle(this);
-            System.out.println("Trójkąt o bokach " + this.sideA + ", " + this.sideB + " i " + this.sideC + " jest trójkątem prostokątnym (" + rightTriangle + ")");
+            System.out.println("Trójkąt o bokach " + sideA + ", " + sideB + " i " + sideC + " jest trójkątem prostokątnym (" + rightTriangle + ")");
         } else {
-            System.out.println("Nie można utworzyć trójkąta o bokach: " + this.sideA + ", " + this.sideB + " i " + this.sideC);
+            System.out.println("Nie można utworzyć trójkąta o bokach: " + sideA + ", " + sideB + " i " + sideC);
         }
     }
 }
